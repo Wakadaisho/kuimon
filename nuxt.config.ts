51c84fc39c5d8dd2ad10033670e94c77976d6e02
Wakadaisho <paulwahome9@gmail.com>
2024-02-22 10:22:08 +0300

    Install Supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/supabase"],
+  app: {
+    head: {
+      title: "Kuimon",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "The Restauranter's Menu",
+        },
+      ],
+    },
+  },
+});
